710d76efbb65ef3534b51b954e5a5858a62a6f67
Ettore <ettorecelozzi@gmail.com>
2017-05-29 21:10:51 +0200

    test completato

--- Ngram.py
+++ Ngram.py
@@ -1,37 +1,3 @@
-'''
-def bigram():  # IMPORTANTE: necessita di uno spazio alla fine della lista dei nomi.
-    l = open("C:\\Users\\Michele\\Desktop\\nomi.txt", "r")
-    linelist = l.readlines()
-    bigramlist = []
-    for j in range(len(linelist)):
-        for i in range(len(linelist[j]) - 2):  # sottrazione dovuta alla lunghezza falsata dal "\n"
-            bigramlist.append(linelist[j][i] + linelist[j][i + 1] + "\n")
-            if i == len(linelist):  # per copiare ultimo elemento eliminando "\n"
-                bigramlist.append(linelist[j][i + 1] + linelist[j][i + 2] + "\n")
-    l.close()
-    l = open("C:\\Users\\Michele\\Desktop\\bigram.txt", "w")
-    for i in range(len(bigramlist)):
-        l.write(bigramlist[i])
-    return bigramlist
-
-
-def trigram():  # IMPORTANTE: necessita di uno spazio alla fine della lista dei nomi.
-    l = open("C:\\Users\\Michele\\Desktop\\nomi.txt", "r")
-    linelist = l.readlines()
-    trigramlist = []
-    for j in range(len(linelist)):
-        for i in range(len(linelist[j]) - 2):
-            if i == (len(linelist[j]) - 3):
-                break
-            trigramlist.append(linelist[j][i] + linelist[j][i + 1] + linelist[j][i + 2] + "\n")
-    l.close()
-    l = open("C:\\Users\\Michele\\Desktop\\trigram.txt", "w")
-    for i in range(len(trigramlist)):
-        l.write(trigramlist[i])
-    return trigramlist
-'''
-
-
 class Ngrams:
     def onegram(parola):
         gram = []
@@ -57,6 +23,5 @@ class Ngrams:
         3: trigram,
     }
 
-
-#ngram = Ngrams()
-#print ngram.option[1]("margherita")
+# ngram = Ngrams()
+# print ngram.option[int('1')]("margherita")
--- Test.py
+++ Test.py
@@ -2,24 +2,89 @@ from Distance import editDistance
 from Ngram import Ngrams
 
 
+# creazione file per bigrammi
+def filebigram():
+    alphabet = __import__("string").lowercase
+    for letter1 in alphabet:
+        for letter2 in alphabet:
+            nomefile = 'file-' + letter1 + letter2 + '.txt'
+            open(nomefile, "w")
+    nomi = open("nomi.txt", "r")
+    for k in nomi:
+        name = k[:len(k) - 1]  # elimino il carattere \n alla fine della mia parola
+        gram = Ngrams.option[2](name)
+        for i in gram:
+            filename = 'file-' + i + '.txt'
+            f = open(filename, "a")
+            f.write(name + "\n")
+
+
+def filetrigram():
+    alphabet = __import__("string").lowercase
+    for letter1 in alphabet:
+        for letter2 in alphabet:
+            for letter3 in alphabet:
+                nomefile = 'file-' + letter1 + letter2 + letter3 + '.txt'
+                open(nomefile, "w")
+    nomi = open("nomi.txt", "r")
+    for k in nomi:
+        name = k[:len(k) - 1]
+        gram = Ngrams.option[3](name)
+        for i in gram:
+            filename = 'file-' + i + '.txt'
+            f = open(filename, "a")
+            f.write(name + "\n")
+
+
 def testedit():
     query = raw_input("Inserire nome da cercare ")
-    nomi = open("C:\\Users\\Michele\\Desktop\\nomi.txt", "r")
+    nomi = open("nomi.txt", "r")
     val = []
     query += "\n"
     for i in nomi:
         m = editDistance(query, i)
         if m <= 1:
             val.append(i)
-    print val
+    print "Forse cercavi:",
+    for i in val:
+        nomi = i[:len(i) - 1]
+        print nomi,
+    print ""
 
 
 def testngram():
-    ngramQuery = []
-    query = raw_input("Inserire nome da cercare")
-    n = raw_input("Inserire n per gli n-gram")
-    ngramQuery.append(Ngrams.option[n](query))  # contiene gli ngrammi della query
-    nomi = open("C:\\Users\\Michele\\Desktop\\nomi.txt", "r")
-    tmp = []
-    for nome in nomi:
-        tmp.append(Ngrams.option[n](nome))
+    query = raw_input("Inserire nome da cercare:")
+    n = raw_input("Inserire n per gli n-gram:")
+    if int(n) == 2:
+        ngram = "bi"
+    if int(n) == 3:
+        ngram = "tri"
+    lunghezzaedit = raw_input("Inserire valore di riferimento Edit_Distance:")
+    word = []
+    ngramQuery = Ngrams.option[int(n)](query)  # contiene gli ngrammi della query
+    for i in range(1, len(ngramQuery)):
+        nomefl = 'C:\\Users\\Michele\\PycharmProjects\\Es6\\' + ngram + 'grammi\\file-' + ngramQuery[i] + '.txt'
+        l = open(nomefl, "r")
+        query += "\n"
+        for k in l:
+            edit = editDistance(query, k)
+            if edit <= int(lunghezzaedit):
+                word.append(k)
+    word = list(set(word))  # elimino le ripetizioni
+    c = open("word.txt", "a")
+    for k in word:
+        c.write(k)
+    word = list(set(word))
+    print "Forse cercavi:",
+    for i in word:
+        nomi = i[:len(i) - 1]
+        print nomi,
+
+
+testngram()
+'''
+query = raw_input("Inserire nome da cercare")
+ngramQuery = Ngrams.option[int(2)](query)  # contiene gli ngrammi della query
+for i in range(1, len(ngramQuery)):
+    print ngramQuery[i]
+'''
